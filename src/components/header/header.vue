6fe28c34f79a04bdd8e5e1770df4602c4e33d81b
ZkEdison <214236543@qq.com>
2017-03-28 02:32:49 +0800

    star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,33 +1,49 @@
 <template>
-  <div class="header">
-    <div class="content-wapper">
-      <div class="avatar">
-        <img :src="seller.avatar" alt="avatar">
-      </div>
-      <div class="content">
-        <div class="title">{{seller.name}}</div>
-        <div class="description">{{seller.description}}/{{seller.deliveryTime}}min arrive</div>
-        <div v-if="seller.supports" class="support">
-          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
-          <span class="text" >{{seller.supports[0].description}}</span>
+  <div>
+    <div class="header">
+      <div class="content-wapper">
+        <div class="avatar">
+          <img :src="seller.avatar" alt="avatar">
+        </div>
+        <div class="content">
+          <div class="title">{{seller.name}}</div>
+          <div class="description">{{seller.description}}/{{seller.deliveryTime}}min arrive</div>
+          <div v-if="seller.supports" class="support">
+            <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+            <span class="text" >{{seller.supports[0].description}}</span>
+          </div>
+        </div>
+        <div class="support-count" v-if="seller.supports">
+          <span class="count" @click="showDetail">{{seller.supports.length}}</span>
+          <i class="icon-keyboard_arrow_right"></i>
         </div>
       </div>
-      <div class="support-count" v-if="seller.supports">
-        <span class="count">{{seller.supports.length}}</span>
+      <div class="bulletin-wrapper" @click="showDetail">
+        {{seller.bulletin}}
         <i class="icon-keyboard_arrow_right"></i>
       </div>
+      <div class="background-header">
+        <img :src="seller.avatar">
+      </div>
     </div>
-    <div class="bulletin-wrapper">
-      {{seller.bulletin}}
-      <i class="icon-keyboard_arrow_right"></i>
-    </div>
-    <div class="background-header">
-      <img :src="seller.avatar">
+    <div class="detail" v-if="detailShow">
+      <div class="detail-wrapper">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="seller.score"></star>
+        </div>
+      </div>
+      <div class="detail-close">
+          <i class="icon-close"></i>
+      </div>
+      
     </div>
   </div>
 </template>
 
 <script>
+import star from './../star/star.vue'
+
 export default {
   name: 'header',
   props: {
@@ -37,11 +53,20 @@ export default {
   },
   data () {
     return {
-      classMap: []
+      classMap: [],
+      detailShow: false
     }
   },
   created () {
     this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+  },
+  methods: {
+    showDetail () {
+      this.detailShow = true
+    }
+  },
+  components: {
+    'star': star
   }
 }
 </script>
---
+++ src/components/star/star.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script>
+const LEGTH = 5
+const CLS_ON = 'on'
+const CLS_HALF = 'half'
+const CLS_OFF = 'off'
+export default {
+  name: 'star',
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    starType () {
+      return 'star-' + this.size
+    },
+    itemClasses () {
+      let rusult = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let intger = Math.floor(score)
+      for (let i = 0; i < intger; i++) {
+        rusult.push(CLS_ON)
+      }
+      if (hasDecimal) {
+        rusult.push(CLS_HALF)
+      };
+      while (rusult.length < LEGTH) {
+        rusult.push(CLS_OFF)
+      }
+      return rusult
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "./../../common/stylus/mixin.styl"
+  .star
+    font-size: 0
+    text-align:center
+    .star-item
+      display:inline-block
+      background-repeat:no-repeat
+      &:last-child
+        margin-right: 0
+    &.star-48
+      .star-item
+        width: 20px
+        height: 20px
+        margin-right: 22px
+        background-size: 20px 20px
+        &.on
+          bg-image('star48_on')
+        &.half
+          bg-image('star48_half')
+        &.off
+          bg-image('star48_off')
+        &:last-child
+          margin-right: 0
+    &.star-36
+      .star-item
+        width: 15px
+        height: 15px
+        margin-right: 6px
+        background-size: 15px 15px
+        &.on
+          bg-image('star36_on')
+        &.half
+          bg-image('star36_half')
+        &.off
+          bg-image('star36_off')
+        &:last-child
+          margin-right: 0
+    &.star-24
+      .star-item
+        width: 10px
+        height: 10px
+        margin-right: 3px
+        background-size: 10px 10px
+        &.on
+          bg-image('star24_on')
+        &.half
+          bg-image('star24_half')
+        &.off
+          bg-image('star24_off')
+        &:last-child
+          margin-right: 0
+    
+</style>
